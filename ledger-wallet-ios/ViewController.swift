c11e7b0561fbead2fc865bcb138aef2981298732
Nicolas Bigot <nicolasbigot@icloud.com>
2015-01-07 10:16:36 +0100

    Initial commit

---
+++ ledger-wallet-ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ledger-wallet-ios
+//
+//  Created by Nicolas Bigot on 07/01/2015.
+//  Copyright (c) 2015 Ledger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
