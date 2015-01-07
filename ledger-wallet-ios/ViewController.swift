4a8448c5f50c1c3067461f5ef431b5a85b6e89ae
Nicolas Bigot <nicolasbigot@icloud.com>
2015-01-07 16:59:36 +0100

    Update project base settings

--- ledger-wallet-ios/ViewController.swift
+++ ledger-wallet-ios/ViewController.swift
@@ -9,10 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
