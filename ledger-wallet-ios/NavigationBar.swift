14dfcc5fac7e351d688255f9c41d17545646e77e
Nicolas Bigot <nicolasbigot@icloud.com>
2015-01-09 16:22:50 +0100

    Add NavigationBar

---
+++ ledger-wallet-ios/NavigationBar.swift
@@ -0,0 +1,38 @@
+//
+//  NavigationBar.swift
+//  ledger-wallet-ios
+//
+//  Created by Nicolas Bigot on 07/01/2015.
+//  Copyright (c) 2015 Ledger. All rights reserved.
+//
+
+import UIKit
+
+class NavigationBar: UINavigationBar {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.configure()
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.configure()
+    }
+    
+    override func configure() {
+        self.translucent = false
+        self.shadowImage = UIImage()
+        self.barTintColor = VisualFactory.Colors.nightBlue
+        self.titleTextAttributes = [
+            NSForegroundColorAttributeName: VisualFactory.Colors.white,
+            NSFontAttributeName: VisualFactory.Fonts.semiboldFontWithSize(15.0)
+        ]
+        setBackgroundImage(UIImage(), forBarMetrics: UIBarMetrics.Default)
+    }
+    
+    override func intrinsicContentSize() -> CGSize {
+        return CGSize(width: UIViewNoIntrinsicMetric, height: 80)
+    }
+    
+}
