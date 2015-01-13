b14a943db6d76dc506326be540f767f0141e068c
Nicolas Bigot <nicolasbigot@icloud.com>
2015-01-13 16:40:50 +0100

    Add UIViewController+Init category

---
+++ ledger-wallet-ios/extensions/UIViewController+Init.h
@@ -0,0 +1,17 @@
+//
+//  UIViewController+Init.h
+//  ledger-wallet-ios
+//
+//  Created by Nicolas Bigot on 13/01/2015.
+//  Copyright (c) 2015 Ledger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "UIViewController+Init.h"
+
+@interface UIViewController (Init)
+
++ (NSString *)className;
++ (instancetype)instantiateFromStoryboard:(UIStoryboard *)storyboard;
+
+@end
---
+++ ledger-wallet-ios/extensions/UIViewController+Init.m
@@ -0,0 +1,22 @@
+//
+//  UIViewController+Init.h
+//  ledger-wallet-ios
+//
+//  Created by Nicolas Bigot on 13/01/2015.
+//  Copyright (c) 2015 Ledger. All rights reserved.
+//
+
+#import "UIViewController+Init.h"
+
+@implementation UIViewController (Init)
+
++ (NSString *)className {
+    return [[NSStringFromClass(self) componentsSeparatedByString:@"."] lastObject];
+}
+
++ (instancetype)instantiateFromStoryboard:(UIStoryboard *)storyboard
+{
+    return [storyboard instantiateViewControllerWithIdentifier:NSStringFromClass([self class])];
+}
+
+@end
