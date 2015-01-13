25eed0032a1d03ccb237059360574d288b830ab5
Nicolas Bigot <nicolasbigot@icloud.com>
2015-01-13 16:40:59 +0100

    Add pairing view controllers

---
+++ ledger-wallet-ios/controllers/pairing/PairingAddDongleViewController.swift
@@ -0,0 +1,15 @@
+//
+//  PairingAddDongleViewController.swift
+//  ledger-wallet-ios
+//
+//  Created by Nicolas Bigot on 13/01/2015.
+//  Copyright (c) 2015 Ledger. All rights reserved.
+//
+
+import UIKit
+
+class PairingAddDongleViewController: ViewController {
+    
+    
+    
+}
---
+++ ledger-wallet-ios/controllers/pairing/PairingListViewController.swift
@@ -0,0 +1,15 @@
+//
+//  PairingListViewController.swift
+//  ledger-wallet-ios
+//
+//  Created by Nicolas Bigot on 13/01/2015.
+//  Copyright (c) 2015 Ledger. All rights reserved.
+//
+
+import UIKit
+
+class PairingListViewController: ViewController {
+    
+    
+    
+}
