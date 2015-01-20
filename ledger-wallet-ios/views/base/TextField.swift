abb2a4291f957b866c18e9c3459a7101c8f707c8
Nicolas Bigot <nicolasbigot@icloud.com>
2015-01-20 11:57:18 +0100

    Add Textfield

--- ledger-wallet-ios/views/base/TextField.swift
+++ ledger-wallet-ios/views/base/TextField.swift
@@ -1,8 +1,8 @@
 //
-//  Label.swift
+//  TextField.swift
 //  ledger-wallet-ios
 //
-//  Created by Nicolas Bigot on 13/01/2015.
+//  Created by Nicolas Bigot on 20/01/2015.
 //  Copyright (c) 2015 Ledger. All rights reserved.
 //
 
@@ -10,8 +10,8 @@ import UIKit
 
 @IBDesignable
 
-class Label: UILabel {
-    
+class TextField: UITextField {
+
     //MARK: Style management
     
     override var text: String? {
@@ -19,13 +19,20 @@ class Label: UILabel {
             ViewStylist.stylizeView(self)
         }
     }
-
+    
+    override var placeholder: String? {
+        didSet {
+            ViewStylist.stylizeView(self)
+        }
+    }
+    
     //MARK: Localization
     
     override func awakeFromNib() {
         super.awakeFromNib()
         
         self.text = localizedString(self.text ?? "")
+        self.placeholder = localizedString(self.text ?? "")
     }
     
 }
