6a31684d7d892bd964daee3d596860ed4e805237
Nicolas Bigot <nicolasbigot@icloud.com>
2015-01-28 14:10:13 +0100

    Add EllipticCurve

---
+++ ledger-wallet-ios/helpers/crypto/EllipticCurve.swift
@@ -0,0 +1,18 @@
+//
+//  EllipticCurve.swift
+//  ledger-wallet-ios
+//
+//  Created by Nicolas Bigot on 28/01/2015.
+//  Copyright (c) 2015 Ledger. All rights reserved.
+//
+
+import Security
+import Foundation
+
+class EllipticCurve {
+    
+    class func generateKeyPair(bitSize: UInt = 256) {
+        
+    }
+    
+}
