1473b4ac93c3b4452868975809b75ca5f61e21d5
Nicolas Bigot <nicolasbigot@icloud.com>
2015-01-28 21:53:34 +0100

    Add LoadingIndicator

---
+++ ledger-wallet-ios/views/custom/LoadingIndicator.swift
@@ -0,0 +1,95 @@
+//
+//  LoadingIndicator.swift
+//  ledger-wallet-ios
+//
+//  Created by Nicolas Bigot on 28/01/2015.
+//  Copyright (c) 2015 Ledger. All rights reserved.
+//
+
+import UIKit
+
+class LoadingIndicator: View {
+    
+    var dotsCount: Int = 12 {
+        didSet {
+            generateLayers()
+        }
+    }
+    var dotsSize: CGFloat = 4.0 {
+        didSet {
+            layer.setNeedsLayout()
+        }
+    }
+    private let dotsColor = VisualFactory.Colors.softGrey
+    
+    // MARK: Layers
+    
+    private func generateLayers() {
+        removeLayers()
+        CATransaction.begin()
+        CATransaction.setDisableActions(true)
+        for _ in 0..<dotsCount {
+            let layer = CALayer()
+            layer.bounds = CGRectZero
+            layer.backgroundColor = dotsColor.CGColor
+            self.layer.addSublayer(layer)
+        }
+        CATransaction.commit()
+        layer.setNeedsLayout()
+    }
+    
+    private func removeLayers() {
+        iterateThroughLayers() { layer in
+            layer.removeFromSuperlayer()
+        }
+    }
+    
+    private func iterateThroughLayers(closure: (CALayer) -> Void) {
+        if (layer.sublayers == nil) {
+            return
+        }
+        for layer in self.layer.sublayers as [CALayer] {
+            closure(layer)
+        }
+    }
+    
+    // MARK: Layout
+    
+    override func layoutSublayersOfLayer(layer: CALayer!) {
+        super.layoutSublayersOfLayer(layer)
+        
+        CATransaction.begin()
+        CATransaction.setDisableActions(true)
+        let increment = CGFloat(M_PI * 2.0) / CGFloat(dotsCount)
+        var angle: CGFloat = 0.0
+        let radius = (layer.bounds.size.width - dotsSize) / 2.0
+        iterateThroughLayers() { sublayer in
+            let center = CGPointMake(CGRectGetMidX(layer.bounds), CGRectGetMidY(layer.bounds))
+            sublayer.bounds = CGRectMake(0, 0, self.dotsSize, self.dotsSize)
+            sublayer.position = CGPointMake(center.x + radius * cos(angle), center.y + radius * sin(angle))
+            sublayer.transform = CATransform3DMakeRotation(angle, 0, 0, 1)
+            angle += increment
+        }
+        CATransaction.commit()
+    }
+    
+    // MARK: Initialization
+    
+    private func initialize() {
+        generateLayers()
+        backgroundColor = VisualFactory.Colors.transparent
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        initialize()
+    }
+    
+    override init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        initialize()
+    }
+    
+}
