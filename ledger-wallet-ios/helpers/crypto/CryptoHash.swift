c4908f3c9e3af9f50acf78d2c290c4b5128108ed
Nicolas Bigot <nicolasbigot@icloud.com>
2015-02-02 14:15:31 +0100

    Add SHA namespace to CryptoHash

--- ledger-wallet-ios/helpers/crypto/CryptoHash.swift
+++ ledger-wallet-ios/helpers/crypto/CryptoHash.swift
@@ -10,18 +10,22 @@ import Foundation
 
 class CryptoHash {
     
-    class func SHA256DataFromData(data: NSData) -> NSData {
-        let length = Int(CC_SHA256_DIGEST_LENGTH)
-        var hash = [UInt8](count: length, repeatedValue: 0)
-        CC_SHA256(data.bytes, CC_LONG(data.length), &hash)
-        return NSData(bytes: hash, length: length)
-    }
-    
-    class func SHA512DataFromData(data: NSData) -> NSData {
-        let length = Int(CC_SHA512_DIGEST_LENGTH)
-        var hash = [UInt8](count: length, repeatedValue: 0)
-        CC_SHA512(data.bytes, CC_LONG(data.length), &hash)
-        return NSData(bytes: hash, length: length)
+    class SHA {
+
+        class func SHA256DataFromData(data: NSData) -> NSData {
+            let length = Int(CC_SHA256_DIGEST_LENGTH)
+            var hash = [UInt8](count: length, repeatedValue: 0)
+            CC_SHA256(data.bytes, CC_LONG(data.length), &hash)
+            return NSData(bytes: hash, length: length)
+        }
+        
+        class func SHA512DataFromData(data: NSData) -> NSData {
+            let length = Int(CC_SHA512_DIGEST_LENGTH)
+            var hash = [UInt8](count: length, repeatedValue: 0)
+            CC_SHA512(data.bytes, CC_LONG(data.length), &hash)
+            return NSData(bytes: hash, length: length)
+        }
+
     }
     
 }
