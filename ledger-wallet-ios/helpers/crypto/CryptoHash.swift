cbd2929b02fdff8ed02e0fc6e4435f6767090c05
Nicolas Bigot <nicolasbigot@icloud.com>
2015-02-05 16:55:44 +0100

    Add XOR in CryptoHash + Update CryptoHashTests

--- ledger-wallet-ios/helpers/crypto/CryptoHash.swift
+++ ledger-wallet-ios/helpers/crypto/CryptoHash.swift
@@ -15,6 +15,7 @@ extension Crypto {
         class func SHA256FromData(data: NSData) -> NSData {
             let length = Int(CC_SHA256_DIGEST_LENGTH)
             var hash = [UInt8](count: length, repeatedValue: 0)
+            
             CC_SHA256(data.bytes, CC_LONG(data.length), &hash)
             return NSData(bytes: hash, length: length)
         }
@@ -22,8 +23,24 @@ extension Crypto {
         class func SHA512FromData(data: NSData) -> NSData {
             let length = Int(CC_SHA512_DIGEST_LENGTH)
             var hash = [UInt8](count: length, repeatedValue: 0)
+            
             CC_SHA512(data.bytes, CC_LONG(data.length), &hash)
             return NSData(bytes: hash, length: length)
+        }
+        
+        class func XORFromDataPair(data: NSData, _ otherData: NSData) -> NSData {
+            if (data.length == 0 || otherData.length == 0 || data.length != otherData.length) {
+                return NSData()
+            }
+            
+            let bytesFirst = UnsafePointer<UInt8>(data.bytes)
+            let bytesSecond = UnsafePointer<UInt8>(otherData.bytes)
+            let newData = NSMutableData(length: data.length)!
+            let writableBytes = UnsafeMutablePointer<UInt8>(newData.mutableBytes)
+            for i in 0..<data.length {
+                writableBytes[i] = bytesFirst[i] ^ bytesSecond[i]
+            }
+            return newData
         }
         
     }
