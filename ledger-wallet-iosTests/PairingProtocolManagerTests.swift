13c2eb088894747e8ae0c616eb0c029afd1e3481
Nicolas Bigot <nicolasbigot@icloud.com>
2015-02-06 16:48:58 +0100

    Update PairingProtocolManagerTests

--- ledger-wallet-iosTests/PairingProtocolManagerTests.swift
+++ ledger-wallet-iosTests/PairingProtocolManagerTests.swift
@@ -13,6 +13,8 @@ class PairingProtocolManagerTests: XCTestCase, PairingProtocolManagerDelegate {
     
     let publicKey = Crypto.Encode.dataFromBase16String("04ae218d8080c7b9cd141b06f6b9f63ef3adf7aecdf49bb3916ac7f5d887fc4027bea6fd187b9fa810b6d251e1430f6555edd2d5b19828d51908917c03e3f7c436")
     let privateKey = Crypto.Encode.dataFromBase16String("b208b83b23edfff327bb6e0098eeaa0a5c87a599d5d8b24ff2734d2aac8bbdde")
+    let attestationKey = Crypto.Encode.dataFromBase16String("04e69fd3c044865200e66f124b5ea237c918503931bee070edfcab79a00a25d6b5a09afbee902b4b763ecf1f9c25f82d6b0cf72bce3faf98523a1066948f1a395f")
+
     let webSocketURL = "ws://192.168.2.107:8080"
     let roomName = "holymacaroni"
     let challengeResponse = "abcd"
@@ -28,12 +30,11 @@ class PairingProtocolManagerTests: XCTestCase, PairingProtocolManagerDelegate {
         
         pairingProtocolManager = PairingProtocolManager()
         pairingProtocolManager.delegate = self
-        pairingProtocolManager.setTestKey(Crypto.Key(privateKey: privateKey))
-        pairingProtocolManager.setTestWebSocketURL(webSocketURL)
-        pairingProtocolManager.setEnvironementIsTest(true)
+        pairingProtocolManager.context = PairingProtocolContext(internalKey: Crypto.Key(privateKey: privateKey), attestationKey: Crypto.Key(publicKey: attestationKey))
+        pairingProtocolManager.webSocketURL = webSocketURL
     }
     
-    func testPairingFlow() {
+    func testPairingProtocol() {
         // join
         pairingProtocolManager.joinRoom(roomName)
         pairingProtocolManager.sendPublicKey()
@@ -54,20 +55,8 @@ class PairingProtocolManagerTests: XCTestCase, PairingProtocolManagerDelegate {
         waitForExpectationsWithTimeout(30) { error in
             if (error != nil) { self.stop = true }
         }
-        
-        if (stop == true) { return }
-
-        // create pairing keychain item
-        if (pairingProtocolManager.canCreatePairingItemNamed(dongleName) == false) {
-            XCTFail("already existing pairing keychain item named \(dongleName)")
-            return
-        }
-        if (pairingProtocolManager.createNewPairingItemNamed(dongleName) == false) {
-            XCTFail("unable to create pairing keychain item named \(dongleName)")
-            return
-        }
     }
-
+    
     override func tearDown() {
         super.tearDown()
         
