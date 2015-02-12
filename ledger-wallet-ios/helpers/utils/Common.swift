b1d561c5ca8ccfc5af3e6350b6b416975e299439
Nicolas Bigot <nicolasbigot@icloud.com>
2015-02-12 17:16:42 +0100

    Add inDebugMode() in Common

--- ledger-wallet-ios/helpers/utils/Common.swift
+++ ledger-wallet-ios/helpers/utils/Common.swift
@@ -16,3 +16,12 @@ let LedgerHelpCenterURL = "http://support.ledgerwallet.com"
 // Attestation keys
 let LedgerDongleAttestationBase16Key = "04e69fd3c044865200e66f124b5ea237c918503931bee070edfcab79a00a25d6b5a09afbee902b4b763ecf1f9c25f82d6b0cf72bce3faf98523a1066948f1a395f"
 let LedgerDongleAttestationKeyData = Crypto.Encode.dataFromBase16String(LedgerDongleAttestationBase16Key)
+
+// Common functions
+func inDebugMode() -> Bool {
+#if DEBUG
+    return true
+#else
+    return false
+#endif
+}
