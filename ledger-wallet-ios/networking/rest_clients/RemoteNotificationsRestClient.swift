328b35414c10d508cad8e5faef982219575a98f6
Nicolas Bigot <nicolasbigot@icloud.com>
2015-02-23 11:04:25 +0100

    Fix RemoteNotificationsRestClient push token registration

--- ledger-wallet-ios/networking/rest_clients/RemoteNotificationsRestClient.swift
+++ ledger-wallet-ios/networking/rest_clients/RemoteNotificationsRestClient.swift
@@ -13,7 +13,7 @@ class RemoteNotificationsRestClient: BaseRestClient {
     // MARK: - Push token management
     
     func registerDeviceToken(token: NSData, toPairingId pairingId: String, completion: ((Bool) -> Void)?) {
-        post("/2fa/pairings/push_token", parameters: ["pairing_id": pairingId, "push_token": Crypto.Encode.base16StringFromData(token)]) { data, request, response, error in
+        post("/2fa/pairings/\(pairingId)/push_token", parameters: ["push_token": Crypto.Encode.base16StringFromData(token)]) { data, request, response, error in
             completion?(error == nil && response != nil)
         }
     }
