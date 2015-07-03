f76920f68e04bf5e233f9f37e788586b1671d349
Nicolas Bigot <nicolasbigot@icloud.com>
2015-07-03 10:37:24 +0200

    Add `deferOnMainQueue` blocks method

--- ledger-wallet-ios/Helpers/Utils/Blocks.swift
+++ ledger-wallet-ios/Helpers/Utils/Blocks.swift
@@ -8,6 +8,10 @@
 
 import Foundation
 
+func deferOnMainQueue(closure: () -> ()) {
+    delayOnMainQueue(0, closure)
+}
+
 func delayOnMainQueue(delay: Double, closure: () -> ()) {
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(delay * Double(NSEC_PER_SEC))), dispatch_get_main_queue(), closure)
 }
