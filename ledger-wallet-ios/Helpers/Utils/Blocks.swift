012e6b35ec3088f46c981fe49df84b69a4ca536d
Nicolas Bigot <nicolasbigot@icloud.com>
2015-07-06 09:48:32 +0200

    Revert "Add `deferOnMainQueue` blocks method"
    This reverts commit f76920f68e04bf5e233f9f37e788586b1671d349.

--- ledger-wallet-ios/Helpers/Utils/Blocks.swift
+++ ledger-wallet-ios/Helpers/Utils/Blocks.swift
@@ -8,10 +8,6 @@
 
 import Foundation
 
-func deferOnMainQueue(closure: () -> ()) {
-    delayOnMainQueue(0, closure)
-}
-
 func delayOnMainQueue(delay: Double, closure: () -> ()) {
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(delay * Double(NSEC_PER_SEC))), dispatch_get_main_queue(), closure)
 }
