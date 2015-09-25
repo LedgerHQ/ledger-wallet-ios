23e8ba297446b8009dae566044dc0dcd2f40b7cb
Nicolas Bigot <nicolasbigot@icloud.com>
2015-09-25 09:59:22 +0200

    Implement new Block features

--- ledger-wallet-ios/Helpers/Utils/Blocks.swift
+++ ledger-wallet-ios/Helpers/Utils/Blocks.swift
@@ -8,14 +8,30 @@
 
 import Foundation
 
-func delayOnMainQueue(delay: Double, closure: () -> ()) {
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(delay * Double(NSEC_PER_SEC))), dispatch_get_main_queue(), closure)
+func dispatchMainQueue() -> dispatch_queue_t {
+    return dispatch_get_main_queue()
 }
 
-func dispatchAsyncOnMainQueue(closure: () -> ()) {
-    dispatch_async(dispatch_get_main_queue(), closure)
+func dispatchGlobalQueueWithPriority(priority: Int) -> dispatch_queue_t {
+    return dispatch_get_global_queue(priority, 0)
 }
 
-func dispatchSyncOnMainQueue(closure: () -> ()) {
-    dispatch_sync(dispatch_get_main_queue(), closure)
+func dispatchAsyncOnMainQueue(block: () -> Void) {
+    dispatch_async(dispatchMainQueue(), block)
+}
+
+func dispatchSyncOnMainQueue(block: () -> Void) {
+    dispatch_sync(dispatchMainQueue(), block)
+}
+
+func delayOnMainQueueAfter(delay: Double, block: () -> Void) {
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(delay * Double(NSEC_PER_SEC))), dispatchMainQueue(), block)
+}
+
+func dispatchAsyncOnGlobalQueueWithPriority(priority: Int, block: () -> Void) {
+    dispatch_async(dispatchGlobalQueueWithPriority(priority), block)
+}
+
+func dispatchSyncOnGlobalQueueWithPriority(priority: Int, block: () -> Void) {
+    dispatch_sync(dispatchGlobalQueueWithPriority(priority), block)
 }
